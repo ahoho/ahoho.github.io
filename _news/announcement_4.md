1c64cf5651aaa67738ee8a2e84984573d4721179
ahoho <ahoyle@wesleyan.edu>
2023-01-12 14:12:01 -0500

    move to html-only with python builder file

--- _news/announcement_4.md
+++
@@ -1,7 +0,0 @@
----
-layout: post
-date: 2020-06-01 16:00:00-0100
-inline: true
----
-
-Starting a research internship at the [Allen Institute for AI](https://allenai.org/), working with [Ana Marasović](https://allenai.org/) and [Noah Smith](https://allenai.org/).
---
+++ build_html.py
@@ -0,0 +1,41 @@
+from itertools import groupby
+
+import jinja2
+from bibtexparser.bparser import BibTexParser
+from bibtexparser.customization import author, splitname
+
+def parse_authors(record):
+    record = author(record) 
+    if "author" in record:
+        record["author_list"] = []
+        for a in record["author"]:
+            record["author_list"].append({k: " ".join(v) for k, v in splitname(a).items()})
+    return record
+
+def parse_bibtex(infile):
+    parser = BibTexParser()
+    parser.customization = parse_authors
+    parser.common_strings = False
+    with open(infile) as infile:
+        bib_db = parser.parse_file(infile)
+
+    entries = sorted(bib_db.entries, key=lambda d: -int(d['year']))
+    publications = {
+        year: list(pubs) for year, pubs in groupby(entries, lambda d: d['year'])
+    }
+    return publications
+
+if __name__ == "__main__":
+    environment = jinja2.Environment(loader=jinja2.FileSystemLoader("templates/"))
+    environment.trim_blocks = True
+    environment.lstrip_blocks = True
+    environment.keep_trailing_newline = False
+
+    pub_template = environment.get_template("pubs.html")
+    publications = parse_bibtex("assets/bibliography/papers.bib")
+    content = pub_template.render(
+        publications=publications,
+        last_name="Hoyle",
+    )
+    with open("main.html", "w") as outfile:
+        outfile.write(content)
